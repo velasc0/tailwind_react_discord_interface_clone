2d43000cd6fe9bcc8c6dc289613b20f5c32f266c
Velasc0 <joao.victor_velasco@hotmail.com>
2022-10-01 15:20:30 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import SideBar from "./components/SideBar/SideBar";
+import ChannelBar from "./components/ChannelBar/ChannelBar";
+import ContentContainer from "./components/ContentContainer/ContentContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <SideBar />
+      <ChannelBar />
+      <ContentContainer />
     </div>
   );
 }
